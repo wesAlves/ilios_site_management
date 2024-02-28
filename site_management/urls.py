c029dad99d43f42e434914ca118087c2de6f0cc4
Wes <wal2.designer@gmail.com>
2024-02-28 20:39:10 +0000

    Fixing url

--- site_management/urls.py
+++ site_management/urls.py
@@ -8,7 +8,7 @@ from . import views
 app_name = "site_management"
 
 router = SimpleRouter()
-router.register(r"", views.SiteManagementView, basename="site_management")
+router.register(r"site-management", views.SiteManagementView, basename="site_management")
 
 urlpatterns = []
 
