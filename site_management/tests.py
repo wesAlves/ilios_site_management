f3f8a63663538eedfb71e75273c5e95e66848f99
Wes <wal2.designer@gmail.com>
2024-02-29 11:34:30 +0000

    creating some tests

--- site_management/tests.py
+++ site_management/tests.py
@@ -1,10 +1,54 @@
 """Testes for site management"""
 
 from django.test import TestCase
+from .models import AvailableSite
 
 
 class TestSiteManagement(TestCase):
     """Testes for site management"""
 
-    def test_base_one(self) -> None:
-        self.assertEqual(1, 1)
+    @classmethod
+    def setUpTestData(cls):
+
+        test_sites = {
+            "name": "Site test",
+            "desctiption": "This a test site and should be temporary",
+            "domain": "example.com",
+            "atrributes": "none",
+            "theme": "none.theme",
+            "base_template": "template one",
+            "author": "Wes",
+        }
+
+        for i in range(10):
+
+            AvailableSite.objects.create(
+                name=f"Site test {i}",
+                desctiption="This a test site and should be temporary",
+                domain="example.com",
+                atrributes="none",
+                theme="none.theme",
+                base_template="template one",
+                author="Wes",
+            )
+
+    def test_list_all_sites(self):
+        sites = AvailableSite.objects.all()
+        self.assertEqual(len(sites), 10)
+
+    def test_list_site_by_id(self):
+        site = AvailableSite.objects.get(id=3)
+        self.assertEqual(site.name, "Site test 2")
+
+    def test_post_a_site(self):
+        pass
+
+    def test_update_a_site(self):
+        pass
+
+    def test_delete_a_site(self):
+        pass
+
+
+class TestPageManagement(TestCase):
+    pass
